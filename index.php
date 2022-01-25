73c93c54e951ab4f481e86950df1dbe809014a18
Doniws <doniwahsap@gmail.com>
2022-01-25 17:45:46 +0700

    Belajar array

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://doniws.github.io/Font-Text/Din/Din.css">
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php
 
-echo "Hello World";
-echo "<h1>Doni Wahyu Saputra</h1>";
+    $item = ["Hello" , "World" , "PHP"];
 
-?>
+    unset($item[2]);
+
+
+    for($i=0; $i < count($item); $i++){
+        echo $item[$i]."<br>";
+    }
+
+
+
+    ?>
+</body>
+</html>
