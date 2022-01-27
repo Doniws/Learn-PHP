9c569f4d0d810d0834d7e45516d22efc7b9bd1a7
Doniws <doniwahsap@gmail.com>
2022-01-27 15:55:31 +0700

    Belajar Operator

--- index.php
+++ index.php
@@ -4,22 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Learn PHP</title>
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://doniws.github.io/Font-Text/Din/Din.css">
 </head>
 <body>
     <h1>Hello</h1>
     <?php
+    $total_belanja = 150000;
 
-    $item = ["Hello" , "World" , "PHP"];
+if($total_belanja < 1000){
+    echo "Anda dapat hadiah!";
+}else{
+    echo "Tidak mendapatkan apa apa";
+}
 
-    unset($item[2]);
-
-
-    for($i=0; $i < count($item); $i++){
-        echo $item[$i]."<br>";
-    }
 
 
 
