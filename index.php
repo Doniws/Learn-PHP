d34fa7eb94fcc1b0e1bfbd4f4162b639f05bc265
Doniws <doniwahsap@gmail.com>
2022-01-29 15:32:01 +0700

    Belajar Percabangan

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Learn PHP</title>
+    <link rel="shortcut icon" href="images/favico.ico" type="image/x-icon">
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://doniws.github.io/Font-Text/Din/Din.css">
 </head>
@@ -12,16 +13,17 @@
     <h1>Hello</h1>
     <?php
 
-    class Doni{
-        function Nama(){
-            echo "NAMA NYA : ". __CLASS__;
+    $kelas = 10 ;
+    $age = 12 ;
+    if ($age > 14.5) {
+        if ($kelas) {
+            echo "Sangat Cukup";
+        }else{
+            echo "Sudah Cukup";
         }
+    }else{
+        echo "Ngak Cukup";
     }
-
-
-    $aku = new Doni();
-    $aku->Nama();
-
     ?>
 </body>
 </html>
